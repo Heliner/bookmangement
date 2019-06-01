41820c790d2aea9927ce82b667c645500bce16d3
hf <303567469@qq.com>
2019-06-01 20:20:13 +0800

    1、更新SQl文件 2、增加ReadME.md

--- README.md
+++ README.md
@@ -1,2 +1,110 @@
-# bookmangement
+# 图书管理系统（bookmangement）
 重庆理工大学专业实习 微信+Web：图书管理系统
+
+# 功能介绍
+
+## 服务器
+
+- 图书查询功能模块：不需要登录，可以查询图书的相关信息
+- 读者功能模块：登录、图书查询、个人信息查看和维护
+- 图书管理员功能：登录、读者信息查询、借书、还书、图书信息管理（增删改查，包括批量导入），打印图书二维码（可选）
+- 系统管理员模块：登录、用户管理（增删改查，包括批量导入）、角色管理（增删改查）、权限管理（增删改查），打印用户借阅证（可选）
+
+## 移动端
+
+- 图书查询功能模块：不需要登录，可以查询图书的相关信息
+- 读者移动端：登录、图书查询、个人信息查看和维护
+- 图书管理员移动端：登录、读者信息查询、借书、还书、图书信息管理（增删改查）
+- 系统管理员移动端：登录、用户管理（增删改查，包括批量导入）、角色管理（增删改查）、权限管理（增删改查）
+
+
+
+# 界面展示
+
+
+
+## 微信端
+
+
+
+![登陆页面](.\ImageForMD\1559390299307.png)
+
+<center>登陆界面</center>
+
+![图书搜索](.\ImageForMD\1559390412395.png)
+
+<center>图书搜索</center>
+
+
+
+## 网页端
+
+![1559389830677](.\ImageForMD\1559389830677.png)
+
+<center>登陆界面</center>
+
+![搜索结果](./ImageForMD/localhost_8080_searchResult.html.png)
+
+<center>搜索结果</center>
+
+![系统管理员](./ImageForMD/localhost_8080_sysAdmin_.png)
+
+<center>系统管理员</center>
+
+![系统管理员搜索](./ImageForMD/localhost_8080_sysAdmin_adminSearchReader.html.png)
+
+<center>系统管理员搜索</center>
+
+# 配置说明
+
+## 网页前端
+
+Vue中部分模块并未下载完全，需要在联网时运行才行
+
+## 微信前端
+
+微信前端使用小程序开发工具开发
+
+## 数据库
+
+数据库使用的是`mysql`，数据库中的模拟数据存储在`/src/main/resources/bookmanagement.sql`中，需要修改`/src/main/resources/application.yml`中的数据库连接信息
+
+```sql
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : mysql80
+Source Server Version : 50724
+Source Host           : localhost:3306
+Source Database       : bookmanagement
+
+Target Server Type    : MYSQL
+Target Server Version : 50724
+File Encoding         : 65001
+
+Date: 2019-06-01 20:10:32
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for bookadmin
+-- ----------------------------
+DROP TABLE IF EXISTS `bookadmin`;
+CREATE TABLE `bookadmin` (
+  `AdName` varchar(255) DEFAULT NULL,
+  `AdId` varchar(36) NOT NULL,
+  `AdPassword` varchar(255) NOT NULL,
+  `AdPhone` varchar(255) NOT NULL,
+  `AdEmail` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`AdId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+```
+
+# 特殊说明
+
+`HELP.md`中为服务端与客户端的通信协议
+
+项目暂时还未部署演示地址，后面可能会考虑加入
+
+微信前端的代码使用过上一届的学长获得。
--- src/main/resources/bookmanagement.sql
+++ src/main/resources/bookmanagement.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50724
 File Encoding         : 65001
 
-Date: 2019-05-27 10:42:13
+Date: 2019-06-01 20:10:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,16 +25,17 @@ CREATE TABLE `bookadmin` (
   `AdPassword` varchar(255) NOT NULL,
   `AdPhone` varchar(255) NOT NULL,
   `AdEmail` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`AdId`),
-  UNIQUE KEY `AdName` (`AdName`)
+  PRIMARY KEY (`AdId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of bookadmin
 -- ----------------------------
-INSERT INTO `bookadmin` VALUES ('管理员3', '123', '1243', '4123', '678');
-INSERT INTO `bookadmin` VALUES ('管理员2', '1234', '123', '123', '1234');
-INSERT INTO `bookadmin` VALUES ('管理云3', '12344', '', '', null);
+INSERT INTO `bookadmin` VALUES ('管理2', '123', '123', '157233', '30356@qq.com');
+INSERT INTO `bookadmin` VALUES ('管理云3', '12343', '123', '1572331', 'h3035@gmail.com');
+INSERT INTO `bookadmin` VALUES ('hfl', '62370db7-82e1-11e9-835d-f48e38ea1b0a', '123', '157', '3035');
+INSERT INTO `bookadmin` VALUES ('hfl', 'c6f72112-82c6-11e9-835d-f48e38ea1b0a', '123', '', '30356@qq.com');
+INSERT INTO `bookadmin` VALUES ('测试用户', 'test', 'test', '1008', 'h30@qq.com');
 
 -- ----------------------------
 -- Table structure for bookinfo
@@ -58,9 +59,14 @@ CREATE TABLE `bookinfo` (
 -- ----------------------------
 -- Records of bookinfo
 -- ----------------------------
-INSERT INTO `bookinfo` VALUES ('', '小王子', '', null, '0.00', '', '0000-00-00', '', '1', '', '0000-00-00');
-INSERT INTO `bookinfo` VALUES ('123', 'xiwaognzi ', '123', 'gihjl', '12.30', '13', '0000-00-00', '', '1', '', '0000-00-00');
+INSERT INTO `bookinfo` VALUES ('3c91bcd5-3b77-453f-9718-294405ae1c6d', '哈利波特', '不是哈利波特', '不知道', '123.40', '不知道', '2018-06-28', '人教出版社', '0', '管理员', '2019-05-30');
+INSERT INTO `bookinfo` VALUES ('5b8f6243-1ee4-4485-b174-45c8dc91d449', 'Spring源码深度解析 第2版', '郝佳', '', '85.10', '9787115499141', '2019-05-08', '人民邮电出版社', '1', '管理员', '2019-05-30');
+INSERT INTO `bookinfo` VALUES ('5dcb50b3-cb0c-415e-92e0-482e02c8ed29', '拯救温斯洛', '莎朗·克里奇', '', '26.40', '9787530590690', '2019-05-09', '天津人民美术出版社', '1', '管理员', '2019-05-30');
+INSERT INTO `bookinfo` VALUES ('6e21b299-73e6-4a9d-b0a4-7319391c2000', '深入理解Java虚拟机：JVM高级特性与最佳实践（第2版）', '周志明', '', '72.70', '9787111421900', '2019-05-08', '机械工业出版社', '1', '管理员', '2019-05-30');
+INSERT INTO `bookinfo` VALUES ('8d882e5b-480f-4493-a9ef-30a578f47d67', '政治的逻辑--马克思主义政治学原理', '王沪宁 林尚立 孙关宏', '', '62.60', '9787208052734', '2019-05-16', '人民出版社', '1', '管理员', '2019-05-30');
 INSERT INTO `bookinfo` VALUES ('c1028f06-7d73-41dd-aa59-aa7d416954c3', '哈利波特', '1', '无', '10.30', 'c1028f06-7d73-41dd-aa59-aa7d416954c3', '2019-05-04', '出版社', '1', '管理员吧', '2019-05-26');
+INSERT INTO `bookinfo` VALUES ('c6d5af42-d237-4305-9694-d84dd8dd411d', '许愿地球仪', '克莉丝汀娜·奥森', '', '19.50', '9787550028715', '2019-05-09', '洲文艺出版社', '1', '管理员', '2019-05-30');
+INSERT INTO `bookinfo` VALUES ('cfdc538b-1a13-4bbf-b8cd-7af2f6f0080e', '侠客岛对话郑永年', '人民日报海外版“侠客岛', '', '48.80', '9787010205786', '2019-05-15', '人民出版社', '1', '管理员', '2019-05-30');
 INSERT INTO `bookinfo` VALUES ('df0ccd73-cdcd-4178-a3cf-2e76cacd830f', '小王子', '作者是我', '译者不是我', '10.30', 'df0ccd73-cdcd-4178-a3cf-2e76cacd830f', '2019-05-02', '人名出版社', '1', '管理员吧', '2019-05-26');
 
 -- ----------------------------
@@ -79,12 +85,18 @@ CREATE TABLE `borrowrecords` (
   KEY `book_id` (`book_id`),
   CONSTRAINT `borrowrecords_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
   CONSTRAINT `borrowrecords_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `bookinfo` (`book_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of borrowrecords
 -- ----------------------------
-INSERT INTO `borrowrecords` VALUES ('4', 'h3035', '123', '2019-06-05', '2019-05-26');
+INSERT INTO `borrowrecords` VALUES ('9', '123', '3c91bcd5-3b77-453f-9718-294405ae1c6d', '2019-06-09', '2019-05-30');
+INSERT INTO `borrowrecords` VALUES ('10', '123', '5dcb50b3-cb0c-415e-92e0-482e02c8ed29', '2019-06-09', '2019-05-30');
+INSERT INTO `borrowrecords` VALUES ('11', '123', '5dcb50b3-cb0c-415e-92e0-482e02c8ed29', '2019-06-09', '2019-05-30');
+INSERT INTO `borrowrecords` VALUES ('12', '123', '5b8f6243-1ee4-4485-b174-45c8dc91d449', '2019-06-09', '2019-05-30');
+INSERT INTO `borrowrecords` VALUES ('13', '123', '3c91bcd5-3b77-453f-9718-294405ae1c6d', '2019-06-09', '2019-05-30');
+INSERT INTO `borrowrecords` VALUES ('14', 'test', '3c91bcd5-3b77-453f-9718-294405ae1c6d', '2019-06-09', '2019-05-30');
+INSERT INTO `borrowrecords` VALUES ('15', 'test', '3c91bcd5-3b77-453f-9718-294405ae1c6d', '2019-06-10', null);
 
 -- ----------------------------
 -- Table structure for systemadmin
@@ -103,6 +115,8 @@ CREATE TABLE `systemadmin` (
 -- ----------------------------
 -- Records of systemadmin
 -- ----------------------------
+INSERT INTO `systemadmin` VALUES ('123', 'hfl', '123', '157233', '303569@qq.com');
+INSERT INTO `systemadmin` VALUES ('test', '测试用户', 'test', '10086', '3035@qq.com');
 
 -- ----------------------------
 -- Table structure for user
@@ -111,7 +125,7 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `user_id` varchar(36) NOT NULL,
   `user_name` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT '123',
   `email` varchar(255) DEFAULT NULL,
   `max` int(11) DEFAULT NULL COMMENT '借阅数量上限',
   `time` int(11) DEFAULT '10' COMMENT '借阅期限上限',
@@ -125,17 +139,10 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('-', null, null, null, null, '10', '0', null, null, null);
-INSERT INTO `user` VALUES ('0', null, null, null, null, '10', '0', null, null, null);
-INSERT INTO `user` VALUES ('1', null, null, null, null, '10', '0', null, null, null);
-INSERT INTO `user` VALUES ('12', null, null, null, null, '10', '0', null, null, null);
-INSERT INTO `user` VALUES ('123', 'hfl', '123', '303567469@qq.com', '10', '10', '1', '67890', 'nm,jkbn', 'ujknmg,as');
-INSERT INTO `user` VALUES ('3', '1', null, null, null, '10', '0', null, null, null);
-INSERT INTO `user` VALUES ('4', null, null, null, null, '10', '0', null, null, null);
-INSERT INTO `user` VALUES ('5', null, null, null, null, '10', '0', null, null, null);
-INSERT INTO `user` VALUES ('6', null, null, null, null, '10', '0', null, null, null);
-INSERT INTO `user` VALUES ('7', null, null, null, null, '10', '0', null, null, null);
-INSERT INTO `user` VALUES ('8', null, null, null, null, '10', '0', null, null, null);
-INSERT INTO `user` VALUES ('9', null, null, null, null, '10', '0', null, null, null);
-INSERT INTO `user` VALUES ('h3035', '韩福林', '', '303567469@qq.com', '10', '30', '-5', '157', '计算机科学与技术', '计算机');
-INSERT INTO `user` VALUES ('h303567469', '韩福林', '', '303567469@qq.com', '10', '30', '0', '157', '计算机科学与工程', '计算机');
+INSERT INTO `user` VALUES ('123', 'hf', '123', '3035674@qq.com', '10', '10', '0', '10086', '计算机科学与技术', '计算机');
+INSERT INTO `user` VALUES ('417e9d87-82ac-11e9-835d-f48e38ea1b0a', 'hfl', '123456', '3035674@qq.com', '10', '30', '0', '10086', '计算机科学', '计算机');
+INSERT INTO `user` VALUES ('bb28ad5b-82e1-11e9-835d-f48e38ea1b0a', 'hfl1', '123456', '3035674@qq.com', '15', '90', '0', '1', 'sjs', 'jsj');
+INSERT INTO `user` VALUES ('e303567469', '韩福林', '123456', '3035674@qq.com', '10', '30', '0', '157', '计算机科学与工程', '计算机');
+INSERT INTO `user` VALUES ('e5595a28-82c6-11e9-835d-f48e38ea1b0a', '韩福林', '123456', '3035674@qq.com', '10', '30', '0', '', '计算机科学', '计算机');
+INSERT INTO `user` VALUES ('e5595a28-82c6-11e9-835d-f48e38ea1b0b', '李振才', '123456', '3035674@qq.com', '10', '10', '0', '157', 'jsj', 'jsj');
+INSERT INTO `user` VALUES ('test', '测试用户', 'test', '3035674@qq.com', '10', '10', '1', '10086', 'jsj', 'jsj');
